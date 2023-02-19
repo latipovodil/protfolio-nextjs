531e4a18ff0693050e26da30b3363515762d2b82
odilbek <odilbeklatipov131333@gmail.com>
2023-02-19 20:39:15 +0500

    ...

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -7,31 +7,9 @@ export default function Head() {
       <meta property="og:title" content="Odilbek Latipov Portfoliosi" />
       <meta property="og:type" content="website" />
       <link
-        rel="dns-prefetch"
-        href="https://static.agilitycms.com/illustrations/blog-illustrations/benefits-of-next.js.jpg?w=800"
-      />
-      <link
-        rel="dns-prefetch"
-        href="https://static.agilitycms.com/illustrations/blog-illustrations/benefits-of-next.js.jpg?w=800"
-      />
-      <link
-        rel="dns-prefetch"
-        href="https://static.agilitycms.com/illustrations/blog-illustrations/benefits-of-next.js.jpg?w=800"
-      />
-      <link
         rel="preconnect"
         href="https://static.agilitycms.com/illustrations/blog-illustrations/benefits-of-next.js.jpg?w=800"
       />
-      <link
-        rel="preconnect"
-        href="https://static.agilitycms.com/illustrations/blog-illustrations/benefits-of-next.js.jpg?w=800"
-      />
-      
-
-
-      <meta name="selected-link" data-turbo-transient="" />
-
-
       <meta
         name="octolytics-url"
         content="https://static.agilitycms.com/illustrations/blog-illustrations/benefits-of-next.js.jpg?w=800"
@@ -40,19 +18,6 @@ export default function Head() {
       <meta name="user-login" content="odilbekl" />
       <link rel="sudo-modal" href="/sessions/sudo_modal" />
       <meta name="viewport" content="width=device-width" />
-      <link
-        rel="search"
-        type="application/opensearchdescription+xml"
-        href="/opensearch.xml"
-        title="Odilbek Portfolio"
-      />
-      <link
-        rel="fluid-icon"
-        href="https://Odilbek Portfolio.com/fluidicon.png"
-        title="Odilbek Portfolio"
-      />
-      <meta property="fb:app_id" content="1401488693436528" />
-      <meta name="apple-itunes-app" content="app-id=1477376905" />
       <meta
         name="twitter:image:src"
         content="https://static.agilitycms.com/illustrations/blog-illustrations/benefits-of-next.js.jpg?w=800"
@@ -60,10 +25,7 @@ export default function Head() {
       <meta name="twitter:site" content="@Odilbek Portfolio" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content="Odilbek Portfolio" />
-      <meta
-        name="twitter:description"
-        content="Odilbek Latipov Portfolio"
-      />
+      <meta name="twitter:description" content="Odilbek Latipov Portfolio" />
       <meta
         property="og:image"
         content="https://static.agilitycms.com/illustrations/blog-illustrations/benefits-of-next.js.jpg?w=800"
@@ -92,46 +54,10 @@ export default function Head() {
         data-refresh-url="/_alive"
         data-session-id="320776d8f80e59aa0b0b60b9d6dd2ea30f8d015c24448da419e0932d2b18329e"
       />
-
-      <meta
-        name="hostname"
-        content="https://static.agilitycms.com/illustrations/blog-illustrations/benefits-of-next.js.jpg?w=800"
-      />
       <meta name="keyboard-shortcuts-preference" content="all" />
       <meta
         name="expected-hostname"
         content="https://static.agilitycms.com/illustrations/blog-illustrations/benefits-of-next.js.jpg?w=800"
-      />
-      <meta
-        name="enabled-features"
-        content="TURBO_EXPERIMENT_RISKY,IMAGE_METRIC_TRACKING,GEOJSON_AZURE_MAPS"
-      />
-
-      <meta
-        http-equiv="x-pjax-version"
-        content="222fbc6ea7f852a87fc7abd097d99bb16b14951d380ac52bf6e59fe94e7e1276"
-        data-turbo-track="reload"
-      />
-      <meta
-        http-equiv="x-pjax-csp-version"
-        content="1e256041dc1b2b6714ea3821038945ca21078ebf137ba7c8d20236f1fd712a83"
-        data-turbo-track="reload"
-      />
-      <meta
-        http-equiv="x-pjax-css-version"
-        content="fae9dfb48c8b3c00ec6c7f0679e03bc91a5c7fcb12876df97e0918e4fc8aea1b"
-        data-turbo-track="reload"
-      />
-      <meta
-        http-equiv="x-pjax-js-version"
-        content="308ad34c36800f54ca26b7b118bb219d0e166a9cea8ad83dcacc5d7da705db51"
-        data-turbo-track="reload"
-      />
-
-      <meta
-        name="turbo-cache-control"
-        content="no-preview"
-        data-turbo-transient=""
       />
 
       <meta
@@ -148,13 +74,7 @@ export default function Head() {
         name="octolytics-dimension-repository_nwo"
         content="https://static.agilitycms.com/illustrations/blog-illustrations/benefits-of-next.js.jpg?w=800"
       />
-      <meta name="octolytics-dimension-repository_public" content="true" />
-      <meta name="octolytics-dimension-repository_is_fork" content="false" />
-      <meta
-        name="octolytics-dimension-repository_network_root_id"
-        content="577169608"
-      />
-      {/* Contribute to odilbekl/homework-4.3-JS-Dom-ToDo developmentContribute to odilbekl/homework-4.3-JS-Dom-ToDo development */}
+
       <meta
         name="octolytics-dimension-repository_network_root_nwo"
         content="https://static.agilitycms.com/illustrations/blog-illustrations/benefits-of-next.js.jpg?w=800"
@@ -164,10 +84,6 @@ export default function Head() {
         rel="canonical"
         href="https://static.agilitycms.com/illustrations/blog-illustrations/benefits-of-next.js.jpg?w=800"
         data-turbo-transient=""
-      />
-      <meta
-        name="turbo-body-classes"
-        content="logged-in env-production page-responsive"
       />
 
       <meta
@@ -197,15 +113,12 @@ export default function Head() {
         href="https://static.agilitycms.com/illustrations/blog-illustrations/benefits-of-next.js.jpg?w=800"
       />
       <link
-      // odilbekl/homework-4.3-JS-Dom-ToDo
+        // odilbekl/homework-4.3-JS-Dom-ToDo
         rel="icon"
         className="js-site-favicon"
         type="image/svg+xml"
         href="https://static.agilitycms.com/illustrations/blog-illustrations/benefits-of-next.js.jpg?w=800"
       />
-
-      <meta name="theme-color" content="#1e2327" />
-      <meta name="color-scheme" content="light dark" />
       <meta
         name="description"
         content="Bu sayt mening(odilbek_latipov) portfolio uchun yaratgan saytim va bu saytni men Next.js, Redux Toolkit, TypeScript,scss,react,emotion,tailwind va yana ayrim kutubxonalar yordamida tayyorlaganman !!! Va SEO optimisation qilingan sayt.Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, quia dolorum, aperiam exercitationem cumque magni quis error soluta labore fugiat porro nihil id, rem provident neque repellat expedita eveniet beatae. "
